4563a121a44284fe8317e2c10753b050813db98d
DomenicDenicola <domenic@domenicdenicola.com>
2011-07-01 11:50:37 -0400

    Style and JSLint fixes. Only real fix is throw new Error instead of throw a parenthesized string.

--- setImmediate.js
+++ setImmediate.js
@@ -1,40 +1,38 @@
-﻿/*jslint browser: true, unparam: false, sloppy: true, es5: false, vars: false, evil: true, plusplus: true, maxerr: 50, indent: 4 */
+﻿/*jshint curly: true, eqeqeq: true, newcap: true, noarg: true, white: true, browser: true */
 
 /* setImmediate.js
  *
- * A cross-browser(ish) implimentation of setImmediate and clearImmediate
- * It should work full speed in Firefox 3+, Internet Explorer 8+, WebKit (Chrome, Safari) and Opera 9.5+
- * If the browser does NOT support postMessage, it falls back to the slow(i.e. normal) setTimeout/clearTimeout method
+ * A cross-browser(ish) implimentation of setImmediate and clearImmediate:
+ * https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/setImmediate/Overview.html
+ * It should work full speed in Firefox 3+, Internet Explorer 8+, WebKit (Chrome, Safari) and Opera 9.5+.
+ * If the browser does NOT support postMessage, it falls back to the slow (i.e. normal) setTimeout/clearTimeout method.
  * In otherwords, setImmediate and clearImmediate is safe in all browsers.
  *
- * 2011-06-30
  * by Barnes and Noble, LLC and Donavon West
  * Public Domain.
  * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
- * see spec at https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/setImmediate/Overview.html
  */
 
 if (!window.setImmediate) {
 
-	if (window.postMessage) { //for modern browsers
+	if (window.postMessage) { // For modern browsers.
 
 		(function () {
-			var handle = 1, //handle MUST be non-zero
-				immediates = [],
-				sendMessage, //
-				messageName = "com.bn.NobleJS.setImmediate";
+			var handle = 1; // Handle MUST be non-zero, says the spec.
+			var immediates = [];
+			var messageName = "com.bn.NobleJS.setImmediate";
 
 			function handleMessage(event) {
 				if (event.source === window && event.data === messageName) {
 					if (event.stopPropagation) {
-						event.stopPropagation(); //TODO: cross browser?
+						event.stopPropagation(); // TODO: is this cross browser?
 					}
 					if (immediates.length) {
 						var task = immediates.shift();
 						if (task.handler.apply) {
 							task.handler.apply(task.that, task.args);
 						} else {
-							throw ("setImmediate.js: shoot me now! there's no way I'm implimenting an evaluated handler");
+							throw new Error("setImmediate.js: shoot me now! there's no way I'm implementing an evaluated handler!");
 						}
 					}
 				}
@@ -48,7 +46,7 @@ if (!window.setImmediate) {
 			window.setImmediate = function (/*handler[, args]*/) {
 				var handler = arguments[0];
 				var args = [].slice.call(arguments, 1);
-				var task = {handle: handle, handler: handler, args: args, that:this};
+				var task = { handle: handle, handler: handler, args: args, that: this };
 				immediates.push(task);
 				window.postMessage(messageName, "*");
 				return handle++;
@@ -64,13 +62,13 @@ if (!window.setImmediate) {
 			};
 		}());
 
-	} else { //fallback to legacy support for non postMessage browsers
+	} else { // Fallback to legacy support for non-postMessage browsers.
 
 		window.setImmediate = function (/*handler[, args]*/) {
 			var that = this;
 			var handler = arguments[0];
 			var args = [].slice.call(arguments, 1);
-			return setTimeout(function() {
+			return setTimeout(function () {
 				if (handler.apply) {
 					handler.apply(that, args);
 				} else {
@@ -82,5 +80,4 @@ if (!window.setImmediate) {
 		window.clearImmediate = clearTimeout;
 
 	}
-
 }
