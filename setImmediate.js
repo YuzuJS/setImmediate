07a8a106b8d7e6ffc8e290739d772074eb0ecc30
DomenicDenicola <domenic@domenicdenicola.com>
2011-07-11 11:29:52 -0400

    Removing cross-browser stuff.

--- setImmediate.js
+++ setImmediate.js
@@ -2,79 +2,63 @@
 
 /* setImmediate.js
  *
- * A cross-browser implimentation of setImmediate and clearImmediate:
+ * An implementation of setImmediate and clearImmediate:
  * https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/setImmediate/Overview.html
- * It should work full speed in Firefox 3+, Internet Explorer 8+, WebKit (Chrome, Safari) and Opera 9.5+.
- * If the browser does NOT support postMessage, it falls back to the slow (i.e. normal) setTimeout/clearTimeout method.
- * In otherwords, setImmediate and clearImmediate are safe in all browsers.
+ * This version only works in modern browsers: Firefox 4+, Internet Explorer 9+, Chrome, Safari 5+.
+ * In particular, we require support for Object.getPrototypeOf, Object.defineProperties, window.postMessage, and window.addEventListener.
  *
- * Copyright (c) 2011 Barnesandnoble.com, llc and Donavon West
  * Released under MIT license (see MIT-LICENSE.txt)
  */
 
 if (!window.setImmediate) {
 	(function () {
-		// If supported, we should attach to the prototype of window, since that is where setTimeout et al. live.
-		var attachTo = typeof Object.getPrototypeOf === "function" ? Object.getPrototypeOf(window) : window;
+		var handle = 1; // Handle MUST be non-zero, says the spec.
+		var immediates = [];
+		var messageName = "com.bn.NobleJS.setImmediate";
 
-		if (window.postMessage) { // For modern browsers.
-			var handle = 1; // Handle MUST be non-zero, says the spec.
-			var immediates = [];
-			var messageName = "com.bn.NobleJS.setImmediate";
+		function handleMessage(event) {
+			if (event.source === window && event.data === messageName) {
+				event.stopPropagation();
 
-			function handleMessage(event) {
-				if (event.source === window && event.data === messageName) {
-					if (event.stopPropagation) { //TODO: needed?
-						event.stopPropagation();
+				if (immediates.length) {
+					var task = immediates.shift();
+					if (task.handler.apply) {
+						task.handler.apply(task.that, task.args);
+					} else {
+						throw new Error("setImmediate.js: shoot me now! there's no way I'm implementing an evaluated handler!");
 					}
-					if (immediates.length) {
-						var task = immediates.shift();
-						if (task.handler.apply) {
-							task.handler.apply(task.that, task.args);
-						} else {
-							throw new Error("setImmediate.js: shoot me now! there's no way I'm implementing an evaluated handler!");
+				}
+			}
+		}
+		window.addEventListener("message", handleMessage, false);
+
+		// Emulate setTimeout/clearTimeout: attach to the prototype of window as writable, enumerable, and non-configurable.
+		Object.defineProperties(Object.getPrototypeOf(window), {
+			setImmediate: {
+				writable: true,
+				enumerable: true,
+				value: function (handler/*, args */) {
+					var args = [].slice.call(arguments, 1);
+					var task = { handle: handle, handler: handler, args: args, that: this };
+
+					immediates.push(task);
+					window.postMessage(messageName, "*");
+
+					return handle++;
+				}
+			},
+			clearImmediate: {
+				writable: true,
+				enumerable: true,
+				value: function (handle) {
+					for (var i = 0; i < immediates.length; i++) {
+						if (immediates[i].handle === handle) {
+							immediates.splice(i, 1); //remove the task
+							break;
 						}
 					}
 				}
 			}
-			if (window.addEventListener) {
-				window.addEventListener("message", handleMessage, false);
-			} else {
-				window.attachEvent("message", handleMessage);
-			}
-
-			attachTo.setImmediate = function (/*handler[, args]*/) {
-				var handler = arguments[0];
-				var args = [].slice.call(arguments, 1);
-				var task = { handle: handle, handler: handler, args: args, that: this };
-				immediates.push(task);
-				window.postMessage(messageName, "*");
-				return handle++;
-			};
-
-			attachTo.clearImmediate = function (handle) {
-				for (var i = 0; i < immediates.length; i++) {
-					if (immediates[i].handle === handle) {
-						immediates.splice(i, 1); //remove the task
-						break;
-					}
-				}
-			};
-		} else { // Fallback to legacy support for non-postMessage browsers.
-			window.setImmediate = function (/*handler[, args]*/) {
-				var that = this;
-				var handler = arguments[0];
-				var args = [].slice.call(arguments, 1);
-				return setTimeout(function () {
-					if (handler.apply) {
-						handler.apply(that, args);
-					} else {
-						throw ("setImmediate.js: shoot me now! there's no way I'm implimenting an evaluated handler");
-					}
-				}, 0);
-			};
-
-			window.clearImmediate = clearTimeout;
-		}
-	} ());
+		});
+	}());
 }
