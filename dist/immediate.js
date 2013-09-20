a693ce00ddfbb24399372df9aceb0a93776c9e4b
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-09-20 09:11:18 -0400

    mutation build

--- dist/immediate.js
+++ dist/immediate.js
@@ -205,6 +205,7 @@ require.register("immediate/lib/index.js", function(exports, require, module){
 var types = [
     require("./realSetImmediate"),
     require("./nextTick"),
+    require("./mutation"),
     require("./postMessage"),
     require("./messageChannel"),
     require("./stateChange"),
@@ -366,6 +367,34 @@ exports.install = function (t) {
 });
 require.register("immediate/lib/global.js", function(exports, require, module){
 module.exports = typeof global === "object" && global ? global : this;
+});
+require.register("immediate/lib/mutation.js", function(exports, require, module){
+"use strict";
+//based off rsvp
+//https://github.com/tildeio/rsvp.js/blob/master/lib/rsvp/async.js
+var globe = require("./global");
+
+var MutationObserver = globe.MutationObserver || globe.WebKitMutationObserver;
+
+exports.test = function () {
+    return MutationObserver;
+};
+
+exports.install = function (handle) {
+    var observer = new MutationObserver(handle);
+    var element = globe.document.createElement("div");
+    observer.observe(element, { attributes: true });
+
+    // Chrome Memory Leak: https://bugs.webkit.org/show_bug.cgi?id=93661
+    globe.addEventListener("unload", function () {
+        observer.disconnect();
+        observer = null;
+    }, false);
+    return function () {
+        console.log('running');
+        element.setAttribute("drainQueue", "drainQueue");
+    };
+};
 });
 require.alias("immediate/lib/index.js", "immediate/index.js");if (typeof exports == "object") {
   module.exports = require("immediate");
