abc2f5512a9de560edca9d10510fa6efdc0965e8
Johan Sköld <johan@skold.cc>
2014-04-29 21:34:21 -0700

    Fixing invocation issue in Firefox
    Apparently Promise has its own toString function that does the same thing
    as Function.toString, and the latter is incompatible with Promise even
    though it is a function.

--- setImmediate.js
+++ setImmediate.js
@@ -66,7 +66,7 @@
         // Tests for the presence of built-in DOM Promise (living spec as of 29/04/2014). As it has many
         // polyfills available, we double check that this is natively supported.
         if (typeof global.Promise === "function") {
-            var source = Function.prototype.toString.call(global.Promise);
+            var source = global.Promise.toString();
             var isNative = source.indexOf("[native code]") !== -1;
             return isNative;
         }
