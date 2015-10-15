a54f4177d2b80591ab12e215bb8802754664bb54
Dumitru Uzun <contact@duzun.me>
2015-10-16 01:12:33 +0300

    Don't use eval to get the global
    Fixes #49.

--- setImmediate.js
+++ setImmediate.js
@@ -172,4 +172,4 @@
 
     attachTo.setImmediate = setImmediate;
     attachTo.clearImmediate = clearImmediate;
-}(new Function("return this")()));
+}(typeof self === "undefined" ? typeof global === "undefined" ? this : global : self));
