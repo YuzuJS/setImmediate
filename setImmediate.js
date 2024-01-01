4f29d9d4d8d3aac0092078a98e31f2f44b80808a
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2024-01-01 15:47:52 +0900

    ES2015 optional arguments to reduce filesize and improve performance

--- setImmediate.js
+++ setImmediate.js
@@ -11,15 +11,10 @@
     var doc = global.document;
     var registerImmediate;
 
-    function setImmediate(callback) {
+    function setImmediate(callback, ...args) {
       // Callback can either be a function or a string
       if (typeof callback !== "function") {
         callback = new Function("" + callback);
-      }
-      // Copy function arguments
-      var args = new Array(arguments.length - 1);
-      for (var i = 0; i < args.length; i++) {
-          args[i] = arguments[i + 1];
       }
       // Store and register the task
       var task = { callback: callback, args: args };
